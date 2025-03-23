f4121111ed0afe15458d50f7f47cfcbec4241ee4
Thiago Caputi <lcaputi2@gmail.com>
2025-03-23 15:18:40 -0300

    feat: add tarefa 2 'is-prime-number' ao server.js

--- criando_api_node.js_pt1/server.js
+++ criando_api_node.js_pt1/server.js
@@ -7,12 +7,8 @@ const port = 3200;
 const server = createServer((request, response) => {
     response.statusCode= 200;
     response.setHeader('content-Type','application/json'); 
-
     try{
-
     const url = new URL(request.url, `http://${host}:${port}`);
-
-
     if(request.method === 'GET' && url.pathname === '/health-check'){
         response.statusCode=200;
         const now = new Date();
@@ -22,13 +18,35 @@ const server = createServer((request, response) => {
         }));
     }else if(request.method === 'GET' && url.pathname === '/is-prime-number'){
         response.statusCode=200;
-        
-        
-    }else{
+        const number = url.searchParams.get('number');
+        const num = Number(number);
+        if(!num|| isNaN(num) || num <0 ){
+            response.statusCode = 400;
+            return response.end(JSON.stringify({ error: 'Invalid Input'}));
+        }else{
+            
+            function isPrime(num){
+                let contador =0;
+                for(let i=1;i <=num ; i++){
+                    if(num % i ==0){
+                        contador++;
+                    }
+                }
+                if(contador ==2){
+                    return true;
+                }else return false;
+            }
+            const result = isPrime(num);
+            response.statusCode = 200;
+            response.end(JSON.stringify({isPrime:result}));
+        };
+    }else if(request.method === 'POST'){
 
+    }else{
     response.statusCode = 404;
     response.end(JSON.stringify({error:"Route Not Found"}));
     }
+
 }catch(error){
     console.error(error);
     response.statusCode = 500;
