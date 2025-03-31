406c30a6ef40f0e90c67f92a3c487ba1a90eb628
Thiago Caputi <lcaputi2@gmail.com>
2025-03-31 10:44:52 -0300

    feat: add final updates and complete implementation

--- Curso_completo_de_APIs_com_Nodejs,_Express_+_MongoDB,_SQL/criando_primeira_api/index.js
+++ Curso_completo_de_APIs_com_Nodejs,_Express_+_MongoDB,_SQL/criando_primeira_api/index.js
@@ -12,20 +12,46 @@ server.use(express.json());
 
 const cursos = ['JavaScript', 'Java', 'C' , 'Python', 'Node JS', 'Boga'];
 
+
+//Middleware global
+server.use((req,res,next) => {
+    console.log(`URL Chamada${req.url}`);
+    return next();
+});
+
+//Moddleware local
+function checkCurso(req,res,next){
+    if(!req.body.name){
+        return res.status(400).json({error: `Missing 'name' requisition.body`});
+    }
+
+    return next();
+}
+
+function checkIndexCurso(req,res,next){
+    const curso = cursos[req.params.index];
+
+    if(!curso){
+        return res.status(400).json({
+            erro: 'Curso não encontrado'
+        });
+    }
+    req.curso = curso;
+    return next();
+}
+
 //Exibindo a lista dos cursos
 server.get('/cursos', (req,res) =>{
     return res.json(cursos);
 });
 
 //Exibindo um curso específico
-server.get('/cursos/:index',(req,res)=>{
-    const {index} = req.params;
-    
-    return res.json(cursos[+index-1]);
+server.get('/cursos/:index', checkIndexCurso,(req,res)=>{    
+    return res.json(req.curso);
 });
 
 //adcionando um curso ao final da lista
-server.post('/cursos',(req,res)=>{
+server.post('/cursos', checkCurso,(req,res)=>{
     const { name } = req.body;
     cursos.push(name);
 
@@ -33,7 +59,7 @@ server.post('/cursos',(req,res)=>{
 });
 
 //Editando o curso
-server.put('/cursos/:index', (req,res)=>{
+server.put('/cursos/:index', checkCurso,checkIndexCurso,(req,res)=>{
     const {index} = req.params;
     const {name} = req.body;
 
@@ -41,6 +67,17 @@ server.put('/cursos/:index', (req,res)=>{
 
     return res.json(cursos);
 })
+
+//removendo algum curso
+server.delete('/cursos/:index',checkIndexCurso, (req,res)=>{
+    const {index} =req.params;
+
+    const curso_deletado = cursos[index];
+
+    cursos.splice(+index,1);
+    return res.json({menssage: `O curso ${curso_deletado} foi removido com sucesso`})
+})
+
 
 server.listen( 3000, ()=>{
     console.log(`server running at http://localhost:3000/`);
