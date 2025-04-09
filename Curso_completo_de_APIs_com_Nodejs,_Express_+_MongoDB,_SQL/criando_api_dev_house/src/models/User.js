ccafc888345ff50891119c9694e396820453c4e6
CaputiDev <lcaputi2@gmail.com>
2025-04-09 11:20:36 -0300

    Delete Curso_completo_de_APIs_com_Nodejs,_Express_+_MongoDB,_SQL directory

--- Curso_completo_de_APIs_com_Nodejs,_Express_+_MongoDB,_SQL/criando_api_dev_house/src/models/User.js
+++
@@ -1,7 +0,0 @@
-const {Schema, model} = require('mongoose');
-
-const UserSchema = new Schema({
-    email: String,
-});
-
-module.exports = new model('User', UserSchema)
--- Curso_completo_de_APIs_com_Nodejs,_Express_+_MongoDB,_SQL/criando_api_dev_house/src/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-
-const port = 3010;
-
-app.listen(port, ()=>{
-    console.log(`server running at http://localhost:${port}/`)
-})
