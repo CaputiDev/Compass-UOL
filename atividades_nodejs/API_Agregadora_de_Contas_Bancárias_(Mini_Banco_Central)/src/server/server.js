6559680750fe13640deb35a69e288f88672327ac
Thiago Caputi <lcaputi2@gmail.com>
2025-05-28 18:28:56 -0300

    refactor: optimization of all the code

--- atividades_nodejs/API_Agregadora_de_Contas_Bancárias_(Mini_Banco_Central)/src/server/server.js
+++ atividades_nodejs/API_Agregadora_de_Contas_Bancárias_(Mini_Banco_Central)/src/server/server.js
@@ -2,12 +2,10 @@ import app from '../app.js';
 import database from '../database/db.js';
 
 const port = 3050;
-const pgport = 5050;
+
 database.sync({alter:true});
 
 app.listen(port, () => {
     // eslint-disable-next-line
     console.log(`Server running at http://localhost:${port}/`);
-    // eslint-disable-next-line
-    console.log(`pgAdmin running at http://localhost:${pgport}/`)
 });
